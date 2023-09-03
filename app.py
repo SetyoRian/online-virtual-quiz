d0a2ca5aa66746b47d6e47c2198d36d24af7d5a6
SetyoRian <setyo.wahyu.trianto@gmail.com>
2023-09-03 15:34:43 +0700

    change app

--- app.py
+++ app.py
@@ -114,5 +114,5 @@ def hallo():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
